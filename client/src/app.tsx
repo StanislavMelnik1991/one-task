f175c6ce756bf6613ab73b6bb42f3ad6efa7ec41
Anna <Laluna-93@yandex.ru>
2022-04-16 21:40:20 +0400

    feat: add fetch render

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,52 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-export function App(){
-    return <h1>Hel</h1>
+export function App() {
+    return <FileViewer />
+}
+function FileViewer() {
+    //   const [dir, setDir] = useState({})  
+    const [files, setFiles] = useState([])
+    const [path, setPath] = useState('')
+    useEffect(() => {
+        fetch(`http://localhost:3000/${path}`).then(res => res.json()).then(res => {
+            console.log(res)
+            setFiles(res.files)
+        })
+    }, [path]);
+    return <div>
+        <div onClick={() => {
+            setPath((path) => {
+                const r = path.split('/')
+                r.pop()
+                return r.join("/");
+
+            })
+        }
+        }>
+            <span>
+                ..
+            </span>
+        </div>
+
+        {files.map(file => {
+            return <div onClick={() => {
+                if (file.isDirectory) {
+                    setPath((path) => path + '/' + file.name);
+                    // fetch(`http://localhost:3000/${path}`).then(res => res.json()).then(res => {
+                    //     setFiles(res.files)
+                    // })
+                }
+            }
+            }>
+                <span>
+                    {file.name}
+                </span>
+                <span>
+                    {file.isDirectory ? '...' : ''}
+                </span>
+            </div>
+        })
+        }
+    </div>
 }
 
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,4 @@
 import {App} from "./app"
 import ReactDOM from "react-dom";
 ReactDOM.render(App(), document.querySelector('#root'))
-
+fetch('http://localhost:3000').then(res=>res.json()).then(res=>console.log(res))
